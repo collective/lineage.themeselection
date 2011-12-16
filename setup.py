ccb7920beeb2fbe442bdcad5acb9aebbe02d6dee
Jens W. Klein <jens@bluedynamics.com>
2011-12-16 09:39:35 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0'
+version = '1.1'
 shortdesc ="Theme Selection on Lineage Subsites"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
